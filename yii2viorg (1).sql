3f5040c4d7797d709f48fcff1c4efbe326a8b02c
fedor <intel.fzor@gmail.com>
2021-03-03 23:48:32 +0700

    sql db new 2

---
+++ yii2viorg (1).sql
@@ -0,0 +1,232 @@
+-- phpMyAdmin SQL Dump
+-- version 5.0.2
+-- https://www.phpmyadmin.net/
+--
+-- Хост: 127.0.0.1
+-- Время создания: Мар 03 2021 г., 17:47
+-- Версия сервера: 10.4.11-MariaDB
+-- Версия PHP: 7.4.6
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- База данных: `yii2viorg`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `position`
+--
+
+CREATE TABLE `position` (
+  `id` int(11) NOT NULL,
+  `title` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Дамп данных таблицы `position`
+--
+
+INSERT INTO `position` (`id`, `title`) VALUES
+(1, 'Повар'),
+(2, 'Уборщик помещений');
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `team`
+--
+
+CREATE TABLE `team` (
+  `id` int(11) NOT NULL,
+  `title` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Дамп данных таблицы `team`
+--
+
+INSERT INTO `team` (`id`, `title`) VALUES
+(3, 'Повары'),
+(4, 'Без бригады');
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `violations`
+--
+
+CREATE TABLE `violations` (
+  `id` int(11) NOT NULL,
+  `worker_id` int(11) NOT NULL,
+  `date` datetime NOT NULL,
+  `type_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Дамп данных таблицы `violations`
+--
+
+INSERT INTO `violations` (`id`, `worker_id`, `date`, `type_id`) VALUES
+(1, 1, '2021-01-25 00:00:00', 2),
+(2, 2, '2021-02-02 00:57:00', 1),
+(3, 1, '2021-02-02 07:12:00', 1),
+(4, 1, '2021-01-21 05:10:00', 1),
+(5, 1, '2021-02-02 00:40:00', 2),
+(7, 1, '2021-03-03 00:00:00', 3),
+(8, 1, '2021-03-03 00:00:00', 1);
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `vi_type`
+--
+
+CREATE TABLE `vi_type` (
+  `id` int(11) NOT NULL,
+  `title` varchar(250) NOT NULL,
+  `position_id` int(11) DEFAULT NULL,
+  `class` varchar(10) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Дамп данных таблицы `vi_type`
+--
+
+INSERT INTO `vi_type` (`id`, `title`, `position_id`, `class`) VALUES
+(1, 'Грязная форма', NULL, 'general'),
+(2, 'Не наточены ножи', 1, 'spec'),
+(3, 'Отсутствуют перчатки', 2, 'spec');
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `workers`
+--
+
+CREATE TABLE `workers` (
+  `id` int(11) NOT NULL,
+  `fio` varchar(100) NOT NULL,
+  `position_id` int(11) NOT NULL,
+  `team_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Дамп данных таблицы `workers`
+--
+
+INSERT INTO `workers` (`id`, `fio`, `position_id`, `team_id`) VALUES
+(1, 'Шмидт Фёдор Евгеньевич', 2, 4),
+(2, 'Иванов Иван Иванович', 1, 3),
+(3, 'Рабов Андрей Анатольевич', 1, 4);
+
+--
+-- Индексы сохранённых таблиц
+--
+
+--
+-- Индексы таблицы `position`
+--
+ALTER TABLE `position`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Индексы таблицы `team`
+--
+ALTER TABLE `team`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Индексы таблицы `violations`
+--
+ALTER TABLE `violations`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `employee_id` (`worker_id`,`type_id`),
+  ADD KEY `type_id` (`type_id`);
+
+--
+-- Индексы таблицы `vi_type`
+--
+ALTER TABLE `vi_type`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `position_id` (`position_id`);
+
+--
+-- Индексы таблицы `workers`
+--
+ALTER TABLE `workers`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `position_id` (`position_id`,`team_id`),
+  ADD KEY `team_id` (`team_id`);
+
+--
+-- AUTO_INCREMENT для сохранённых таблиц
+--
+
+--
+-- AUTO_INCREMENT для таблицы `position`
+--
+ALTER TABLE `position`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT для таблицы `team`
+--
+ALTER TABLE `team`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT для таблицы `violations`
+--
+ALTER TABLE `violations`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+
+--
+-- AUTO_INCREMENT для таблицы `vi_type`
+--
+ALTER TABLE `vi_type`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT для таблицы `workers`
+--
+ALTER TABLE `workers`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- Ограничения внешнего ключа сохраненных таблиц
+--
+
+--
+-- Ограничения внешнего ключа таблицы `violations`
+--
+ALTER TABLE `violations`
+  ADD CONSTRAINT `violations_ibfk_1` FOREIGN KEY (`worker_id`) REFERENCES `workers` (`id`) ON DELETE NO ACTION ON UPDATE CASCADE,
+  ADD CONSTRAINT `violations_ibfk_2` FOREIGN KEY (`type_id`) REFERENCES `vi_type` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Ограничения внешнего ключа таблицы `vi_type`
+--
+ALTER TABLE `vi_type`
+  ADD CONSTRAINT `vi_type_ibfk_1` FOREIGN KEY (`position_id`) REFERENCES `position` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Ограничения внешнего ключа таблицы `workers`
+--
+ALTER TABLE `workers`
+  ADD CONSTRAINT `workers_ibfk_1` FOREIGN KEY (`position_id`) REFERENCES `position` (`id`) ON DELETE NO ACTION ON UPDATE CASCADE,
+  ADD CONSTRAINT `workers_ibfk_2` FOREIGN KEY (`team_id`) REFERENCES `team` (`id`) ON DELETE NO ACTION ON UPDATE CASCADE;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
